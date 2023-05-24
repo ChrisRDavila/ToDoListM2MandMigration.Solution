bafd71d564693e6670642364ade078626052edec
ChrisRDavila <crossdavila@gmail.com>
2023-05-23 17:54:51 -0700

    add db from TDLContext to home controller

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
 
+      public HomeController(ToDoListContext db)
+      {
+        _db = db;
+      }
+      
       [HttpGet("/")]
       public ActionResult Index()
       {
